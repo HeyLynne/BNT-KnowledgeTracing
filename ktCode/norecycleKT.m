c915cfdd4419981862c5b75696e935daf6e52635
HeyLynne <lion19930924@163.com>
2017-03-31 12:07:52 +0800

    20170331
    delete a file

--- ktCode/norecycleKT.m
+++
@@ -1,5 +0,0 @@
-[data , rows , cols] = loadData('D:\mine_project\code\Matlab\BNT-KT\Question\Q1.txt');
-nS = rows * 0.8;
-nQ = cols;
-[bnet oNodes] = makeBKTmodel(nQ);
-bnetLearned = trainBKTmodel(bnet , data , oNodes);
